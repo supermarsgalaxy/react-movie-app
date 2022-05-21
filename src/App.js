64752859cd38e5486c2915623b66b79fa2ed325b
Mars Brown <meeshajean33@gmail.com>
2022-05-20 20:17:52 -0400

    api working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { useEffect } from "react";
 import React from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=3b8aebab";
+
+const movie1 = {
+  Title: "Shrek",
+  Year: "2001",
+  imdbID: "tt0126029",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+};
+
 const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -12,7 +24,40 @@ const App = () => {
   useEffect(() => {
     searchMovies("Shrek");
   }, []);
-  return <h1>App</h1>;
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for Movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
